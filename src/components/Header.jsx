963a49d6c2dd80bf30ab98fafd692c7ea11fcde1
Szpytma <szpytmapawel@hotmail.com>
2023-04-13 14:33:34 +0100

    added css to item/id and links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom";
+
 function Header() {
-    return( <header>
+  return (
+    <header>
+      <Link to="/items">
         <h1>NC Marketplace</h1>
+      </Link>
     </header>
-    )
+  );
 }
 
 export default Header;
--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 function ItemCard({
   item_id,
   item_name,
@@ -9,7 +11,9 @@ function ItemCard({
   return (
     <div className="ItemCard">
       <li key={item_id}>
-        <h2>{item_name}</h2>
+        <Link to={`/items/${item_id}`}>
+          <h2>{item_name}</h2>
+        </Link>
         <img src={img_url} alt={item_name} />
         <p>$ {price}</p>
         <p>{category_name}</p>
