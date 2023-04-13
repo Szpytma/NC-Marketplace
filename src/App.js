502b43da8c2ad299de38f1cc7ae8c6f39a36ab01
Szpytma <szpytmapawel@hotmail.com>
2023-04-13 12:34:53 +0100

    routing implemented

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import './App.css';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import ItemList from './components/ItemList';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import ItemList from "./components/ItemList";
 
 function App() {
-
-
   return (
-    <div>
-      <Header/>
-      <ItemList/>
-      <Footer/>
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route url="/" element={<ItemList />} />
+      </Routes>
+      <ItemList />
+      <Footer />
     </div>
   );
 }
--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,11 +1,20 @@
-function ItemCard({item_id, item_name, description, img_url, price, category_name}) {
-    return <div>
-        <li key={item_id}>
-            <h2>{item_name}</h2>
-            <img src={img_url} alt={item_name}/>
-            <p>{price}</p>
-            <p>{category_name}</p>
-        </li>
+function ItemCard({
+  item_id,
+  item_name,
+  description,
+  img_url,
+  price,
+  category_name,
+}) {
+  return (
+    <div className="ItemCard">
+      <li key={item_id}>
+        <h2>{item_name}</h2>
+        <img src={img_url} alt={item_name} />
+        <p>{price}</p>
+        <p>{category_name}</p>
+      </li>
     </div>
+  );
 }
 export default ItemCard;
