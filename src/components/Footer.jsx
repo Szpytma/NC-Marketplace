323629523392fc5b1a5707328fb9eb3f7c183f42
Szpytma <szpytmapawel@hotmail.com>
2023-04-14 08:53:59 +0100

    css and footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,16 @@
 function Footer() {
-    const paw = "https://github.com/Szpytma/";
-    const faizan = "https://github.com/SomeRandomGuy64";
-    let year = new Date().getFullYear();
-  
-    return (
-      <footer>
-        Created by{" "}
-        <a href={paw} target="_blank" rel="noreferrer">
-          Paw
-        </a>{" "}
-        and{" "}
-        <a href={faizan} target="_blank" rel="noreferrer">
-          Faizan
-        </a>{" "}
-        ® {year}
-      </footer>
-    );
-  }
-  
-  export default Footer;
+  const paw = "https://github.com/Szpytma/";
+  let year = new Date().getFullYear();
+
+  return (
+    <footer>
+      Created by{" "}
+      <a href={paw} target="_blank" rel="noreferrer">
+        Paw
+      </a>{" "}
+      ® {year}
+    </footer>
+  );
+}
+
+export default Footer;
